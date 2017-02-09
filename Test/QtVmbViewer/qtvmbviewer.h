f080f95e52973d0433802b8bae01fccbdad909bc
Michaël Roy <microygh@gmail.com>
2017-02-09 17:56:17 +0100

    Display the camera image.

--- Test/QtVmbViewer/qtvmbviewer.h
+++ Test/QtVmbViewer/qtvmbviewer.h
@@ -2,24 +2,33 @@
 #define QTVMBVIEWER_H
 
 #include <QWidget>
+#include <QImage>
+#include <QLabel>
 #include "VmbCamera.h"
 
-class QtVmbViewer : public QWidget
-{
+// Qt widget to display the images from an Allied Vision camera
+class QtVmbViewer : public QWidget {
+
     Q_OBJECT
 
-public:
-    QtVmbViewer(QWidget *parent = 0);
-    ~QtVmbViewer();
+    // Public members
+    public :
+        // Constructor
+        QtVmbViewer(QWidget *parent = 0);
+        // Destructor
+        ~QtVmbViewer();
 
-    // Allied Vision camera
-    VmbCamera* camera;
+        // Label to display the camera image
+        QLabel* label;
+        // The camera image
+        QImage* image;
+        // Allied Vision camera
+        VmbCamera* camera;
 
-    // Image callback
-    void ImageCallback();
-
-public slots:
-    void GetFrame( const VmbFrame_t* frame_pointer );
+    // Public slot
+    public slots :
+        // Slot to receive the frame from the camera
+        void GetFrame( const VmbFrame_t* frame_pointer );
 };
 
 #endif // QTVMBVIEWER_H
