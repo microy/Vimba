317bc69c56fc486fab63e112a8992fccace71109
Michaël Roy <microygh@gmail.com>
2017-02-11 16:52:58 +0100

    Switch to PyQt5.

--- QtViewer.py
+++ QtViewer.py
@@ -8,14 +8,15 @@
 # External dependencies
 import sys
 import numpy as np
-from PySide import QtCore
-from PySide import QtGui
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
 import Vimba
 
 # Class to display images from an Allied Vision camera
-class QtVmbViewer( QtGui.QWidget ) :
+class QtVmbViewer( QtWidgets.QWidget ) :
 	# Signal sent to update the image in the widget
-	update_image = QtCore.Signal()
+	update_image = QtCore.pyqtSignal()
 	# Initialization
 	def __init__( self, parent = None ) :
 		# Initialise QWidget
@@ -25,14 +26,14 @@ class QtVmbViewer( QtGui.QWidget ) :
 		# Connect the signal to update the image
 		self.update_image.connect( self.UpdateImage )
 		# Widget to display the images from the cameras
-		self.image_widget = QtGui.QLabel( self )
+		self.image_widget = QtWidgets.QLabel( self )
 		self.image_widget.setScaledContents( True )
 		# Widget layout
-		self.layout = QtGui.QVBoxLayout( self )
+		self.layout = QtWidgets.QVBoxLayout( self )
 		self.layout.addWidget( self.image_widget )
-		self.layout.setSizeConstraint( QtGui.QLayout.SetFixedSize )
+		self.layout.setSizeConstraint( QtWidgets.QLayout.SetFixedSize )
 		# Set the Escape key to close the application
-		QtGui.QShortcut( QtGui.QKeySequence( QtCore.Qt.Key_Escape ), self ).activated.connect( self.close )
+		QtWidgets.QShortcut( QtGui.QKeySequence( QtCore.Qt.Key_Escape ), self ).activated.connect( self.close )
 		# Initialize Vimba
 		Vimba.VmbStartup()
 		# Initialize the camera
@@ -48,8 +49,11 @@ class QtVmbViewer( QtGui.QWidget ) :
 		self.camera.StartCapture(  self.ImageCallback  )
 	# Receive the frame sent by the camera
 	def ImageCallback( self, frame ) :
+		# Get the pointer of the Qt image data
+		image_pointer = self.image.bits()
+		image_pointer.setsize( frame.bufferSize )
 		# Store the camera frame buffer in the Qt image
-		self.image.bits()[ 0 : frame.bufferSize ] = frame.buffer[ 0 : frame.bufferSize ]
+		image_pointer[ 0 : frame.bufferSize ] = frame.buffer[ 0 : frame.bufferSize ]
 		# Update the image in the widget
 		self.update_image.emit()
 	# Process the given image
@@ -70,7 +74,7 @@ class QtVmbViewer( QtGui.QWidget ) :
 		event.accept()
 
 # Main application
-application = QtGui.QApplication( sys.argv )
+application = QtWidgets.QApplication( sys.argv )
 widget = QtVmbViewer()
 widget.show()
 sys.exit( application.exec_() )
